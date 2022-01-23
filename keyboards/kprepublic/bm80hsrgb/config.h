9c9793e00abda466c95cae7e32561fbc488eb6a6
Benn Sundsrud <benn.sundsrud@gmail.com>
2022-01-22 22:28:18 -0600

    rebased bm80 and added bm980 config

--- keyboards/kprepublic/bm80hsrgb/config.h
+++ keyboards/kprepublic/bm80hsrgb/config.h
@@ -140,3 +140,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* disable these deprecated features by default */
 #define NO_ACTION_MACRO
 #define NO_ACTION_FUNCTION
+
+/* enable NKRO by default */
+#define FORCE_NKRO
